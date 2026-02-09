c7f3568e9a1adbbc4bcc37542eaf52ac962c4eb6
Galij Sunuwar <galijsunuwar920@gmail.com>
2026-02-09 23:11:05 +0545

    Fix data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
-export const groceryItems = [
-  { id: "1", name: "milk", completed: true },
-  { id: "2", name: "bread", completed: true },
-  { id: "3", name: "eggs", completed: false },
-  { id: "4", name: "butter", completed: false },
+export const todoList = [
+  { id: "1", name: "Listen to some music", completed: true },
+  { id: "2", name: "Read a book", completed: true },
+  { id: "3", name: "Complete homework", completed: false },
+  { id: "4", name: "Charge your phone", completed: false },
 ];
